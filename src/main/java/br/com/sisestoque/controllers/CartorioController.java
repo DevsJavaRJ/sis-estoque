c64ecb8f49dba5936484550862d4a437eac64de6
Renato Areas <rsareas93@gmail.com>
2022-09-19 18:16:40 -0300

    correção de application.properties

--- src/main/java/br/com/sisestoque/controllers/CartorioController.java
+++ src/main/java/br/com/sisestoque/controllers/CartorioController.java
@@ -1,5 +1,19 @@
 package br.com.sisestoque.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sisestoque.dtos.RequestFuncionarioDto;
+
+@RestController
 public class CartorioController {
+	
+	public ResponseEntity<String> cadastrar (@Validated @RequestBody RequestFuncionarioDto dto){
+		
+		return ResponseEntity.status(HttpStatus.OK).body(null);
+	}
 
 }
